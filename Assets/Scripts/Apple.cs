c1830abbbbb7c6e49033508a717b41949d90ff79
Abroth <71097067+Abroth@users.noreply.github.com>
2021-05-10 20:45:24 -0300

    Manzana y trampas

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
