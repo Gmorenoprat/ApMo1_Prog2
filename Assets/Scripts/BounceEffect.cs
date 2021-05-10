5ed3f9a83833b4601e7891dfbd11444892476c0a
Ariel Ferrer Pitzus <arielferrerxd@gmail.com>
2021-05-09 21:52:15 -0300

    Plataformas con efectos - Gravedad
    Gravedad cambiada a -20
    Cajas con rebote
    Cajas normales con gravedad
    Plataformas que resbalan

---
+++ Assets/Scripts/BounceEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceEffect : MonoBehaviour
+{
+    public float _jumpForce;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+        
+    }
+}
