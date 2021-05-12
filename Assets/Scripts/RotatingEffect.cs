52af39f9507f0366ef1b1e1eb123e84d6ad6e295
Ariel Ferrer Pitzus <arielferrerxd@gmail.com>
2021-05-12 00:12:29 -0300

    Update level
    plataformas, trampas, enemies

---
+++ Assets/Scripts/RotatingEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingEffect : MonoBehaviour
+{
+    Animation Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = this.GetComponent<Animation>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerAttack")
+        {
+            //Anim.SetTrigger("Rotation Active");
+            Anim.Play("rotationActive");
+        }
+    }
+}
