914fdd7c2ee53024603e4366a35c057827aec7cf
juan <jirobert1995@gmail.com>
2021-05-12 02:09:14 -0300

    canvas

---
+++ Assets/Prefabs/Canvas/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseGameUI;
+    public GameObject interfaceUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseGameUI.SetActive(false);
+        interfaceUI.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseGameUI.SetActive(true);
+        interfaceUI.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
---
+++ Assets/Prefabs/Canvas/ScoreText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    public Text appleText;
+    public Text lifeText;
+
+    public Player player;
+
+    void Update()
+    {
+        lifeText.text = player.life.ToString();
+        appleText.text = player.apples.ToString();
+    }
+}
