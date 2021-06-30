3b67c7548f6841b2cf8fed940da7c6f427fc5abd
juan <jirobert1995@gmail.com>
2021-06-30 18:18:45 -0300

    prefab columnas

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public bool isFalling = false;
+    public float downSpeed;
+
+    /// <summary>
+    /// @Author: Robert Juan Ignacio
+    /// </summary>
+    void Update()
+    {
+        Move();
+    }
+    public void Activate()
+    {
+        isFalling = true;
+    }
+    public void Move()
+    {
+        if (isFalling)
+            transform.position += downSpeed * Time.deltaTime * Vector3.down;
+    }
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/FallingPlatformActivation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformActivation : MonoBehaviour
+{
+    public FallingPlatform fallingPlatform;
+
+    /// <summary>
+    /// @Author: Robert Juan Ignacio
+    /// </summary>
+    public void Start()
+    {
+        EventManager.SubscribeToEvent(EventsType.ACTIVATOR_TRIGGER, ActivateFallingPlatform);
+    }
+    public void ActivateFallingPlatform(params object[] parameterContainer)
+    {
+        FallingPlatform fallingPlatform = (FallingPlatform)parameterContainer[0];
+        fallingPlatform.Activate();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        EventManager.TriggerEvent(EventsType.ACTIVATOR_TRIGGER, fallingPlatform);
+    }
+}
