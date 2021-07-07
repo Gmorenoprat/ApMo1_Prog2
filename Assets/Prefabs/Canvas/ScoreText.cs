2a2e9e0319a1048b5bae9d04bb53f347b64b2105
Ariel Ferrer Pitzus <arielferrerxd@gmail.com>
2021-07-07 00:48:44 -0300

    27

---
+++ Assets/Prefabs/Canvas/ScoreText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    public Text appleText;
+    public Text lifeText;
+
+    public Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+    void Update()
+    {
+        lifeText.text = player.life.ToString();
+        appleText.text = player.apples.ToString();
+    }
+}
