c401af98ef9a51a0699e2cec298334e266f009f3
juan <jirobert1995@gmail.com>
2021-07-07 17:02:25 -0300

    menus particulas

---
+++ Assets/Scripts/GroundCheckParticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckParticle : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject dust_particle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            Instantiate(dust_particle, transform.position, dust_particle.transform.rotation);
+        }
+    }
+}
